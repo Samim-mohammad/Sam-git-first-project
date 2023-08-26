d527bc5833dfc54229ad45a10bef147cb665b685
Samim-mohammad <mohammad.samim.habiby@tekschool.us>
2023-08-26 00:05:11 -0700

    third commit to Sam branch

---
+++ src/demo/SecondTestClass.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class SecondTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is inside main branch");
+	}
+
+}
