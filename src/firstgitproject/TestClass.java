740734236b5c5fe5fff1e3db7f4fab5369382f6d
Samim-mohammad <mohammad.samim.habiby@tekschool.us>
2023-08-25 23:01:25 -0700

    Sam adding git project

---
+++ src/firstgitproject/TestClass.java
@@ -0,0 +1,14 @@
+package firstgitproject;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		
+				// TODO Auto-generated method stub
+				System.out.println("Hello Guardian CA");
+
+			}
+
+		}
+
